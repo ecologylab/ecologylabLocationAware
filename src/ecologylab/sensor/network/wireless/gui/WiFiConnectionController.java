b0d13cad8fa2800df77329b4a7d1196420a807a7
toupsz <toupsz@ecologylab.net>
2008-02-19 16:41:50 +0000

    start on GUI controls for reading wifi

---
+++ src/ecologylab/sensor/network/wireless/gui/WiFiConnectionController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ecologylab.sensor.network.wireless.gui;
+
+import stec.jenie.NativeException;
+import ecologylab.sensor.network.wireless.WiFiAdapter;
+
+/**
+ * @author Zachary O. Toups (toupsz@cs.tamu.edu)
+ */
+public interface WiFiConnectionController
+{
+	public boolean connectWiFi(WiFiAdapter newGPS) throws NativeException;
+
+	public void disconnectWiFi();
+
+	/**
+	 * @return the current WiFi adapter object
+	 */
+	public WiFiAdapter getWiFiAdapter();
+}
---
+++ src/ecologylab/sensor/network/wireless/gui/strings/WiFiStatusStrings.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ecologylab.sensor.network.wireless.gui.strings;
+
+/**
+ * @author Zachary O. Toups (toupsz@cs.tamu.edu)
+ *
+ */
+public interface WiFiStatusStrings
+{
+	public static final String CONNECTED_ASSOCIATED = "connected: associated";
+	
+	public static final String CONNECTED_UNASSOCIATED = "connected: unassociated";
+	
+	public static final String DISCONNECTED = "disconnected";
+	
+	public static final String SOFTWARE_FAILURE = "there was an error loading the wifi software";
+}
